34d0ca91f2ee7fb906fd4b918966f0886316ad3f
Alokananda Y <plantsfortrees@gmail.com>
2024-10-10 09:04:48 +0530

    more switch case

---
+++ programming-javaScript/conditional_examples/practice/switch/script.js
@@ -0,0 +1,28 @@
+var day = prompt("Day of the Week? ");
+
+switch (day) {
+  case "Monday":
+    console.log("Start cribbing");
+    break;
+  case "Tuesday":
+    console.log("Jerk off");
+    break;
+  case "Wednesday":
+    console.log("Start thinking about the weekend");
+    break;
+  case "Thursday":
+    console.log("Don't do anything");
+    break;
+  case "Friday":
+    console.log("It's here");
+    break;
+  case "Saturday":
+    console.log("Get drunk");
+    break;
+  case "Sunday":
+    console.log("Regret");
+    break;
+  default:
+    console.log("Fuck off");
+    break;
+}
