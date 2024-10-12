474224243fe1d8fe172f5f24968789d973ad7188
Alokananda Y <plantsfortrees@gmail.com>
2024-10-12 08:58:56 +0530

    Objects

---
+++ programming-javaScript/objects/assistant_manager/script.js
@@ -0,0 +1,13 @@
+var assistantManager = {
+  movement: 3,
+  socialSkills: 30,
+  streetSmarts: 30,
+  health: 40,
+};
+
+var worker = {
+  loyalty: 5,
+  wageFairness: true,
+  streetSmarts: false,
+  punctuality: 50,
+};
---
+++ programming-javaScript/objects/store-manager/script.js
@@ -0,0 +1,12 @@
+var storeManagerMovement = 4;
+var storeManagerSocialSkills = 50;
+var storeManagerStreetSmarts = 50;
+var storeManagerHealth = 30;
+
+var storeManager = {};
+storeManager.rangeTitlesPerTurn = 4;
+storeManager.socialSkills = 50;
+storeManager.streetSmarts = 50;
+storeManager.health = 30;
+storeManager.specialAbility = "finding business opportunities";
+storeManager.greeting = "Let's make some money";
