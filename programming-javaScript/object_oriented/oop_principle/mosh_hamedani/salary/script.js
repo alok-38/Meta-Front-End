842e43b049fcd4dcecbb36555a5559d8c9881331
Alokananda Y <plantsfortrees@gmail.com>
2024-10-15 07:57:20 +0530

    Objected oriented principle

---
+++ programming-javaScript/object_oriented/oop_principle/mosh_hamedani/salary/script.js
@@ -0,0 +1,9 @@
+let baseSalary = 30_000;
+let overtime = 10;
+let rate = 20;
+
+function getWage(baseSalary, overtime, rate) {
+  return baseSalary + overtime * rate;
+}
+
+console.log(getWage(10_000, 20, 20));
---
+++ programming-javaScript/object_oriented/oop_principle/script.js
@@ -0,0 +1,11 @@
+class Animal {}
+
+var myDog = Object.create(Animal);
+
+console.log(Animal);
+
+class FarmAnimal {}
+
+var myPig = new FarmAnimal();
+
+console.log(FarmAnimal);
